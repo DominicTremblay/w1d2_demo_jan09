5ce06b220d6431d877e615dc45964020b13640bb
Dominic Tremblay <dominictremblay24@gmail.com>
2023-01-10 14:02:38 -0500

    adding a readme file and an example of sum

--- sum.js
+++ sum.js
@@ -1,2 +1,3 @@
 // - Write a node program that takes in an unlimited number of command line arguments, goes through each and prints out the sum of them. If any argument is not a whole number, skip it. Do support negative numbers though. If any argument is not a number, output an error message. We need at least 2 arguments.
 
+// ex. node sum.js 1 2 3 4 5 should give 15
